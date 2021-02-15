664f619ce0d5f556d964c4ad429be23c305e5c66
Aubrey Sledzinski <asledzinski88@gmail.com>
2021-02-15 10:46:57 -0500

    Develop

---
+++ src/components/Header.vue
@@ -0,0 +1,37 @@
+<template>
+    <div id="header">
+        <h1>Pokemon Random Generator </h1>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script> 
+
+<style scoped>
+/* 
+light-red = #FF0000
+dark-red = #CC0000
+blue = #3B4CCA
+light-yellow = #FFDE00
+dark - yellow = #B3A125
+
+
+
+ */
+
+#header {
+    position: absolute;
+    top: 0;
+     background: linear-gradient(45deg , #FF0000, #CC0000, #3B4CCA,#FFDE00,#B3A125);
+    width: 100vh;
+}
+
+h1 {
+    color:white;
+}
+</style>
+
+
